bf8a6aa8c9ebb08d43c2d48140227c64a763a727
JackCarlos1990 <ymco0393@gmail.com>
2024-12-22 00:49:21 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
 }
 
+module.exports = nextConfig
